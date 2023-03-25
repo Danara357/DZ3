e4cc47b643ca5394f08700312da2ee8ca32dd8df
Мой первый коммит <dsbasanova@mail.ru>
2023-03-25 23:25:15 +0300

    Задача 8

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        task6();
+        task8();
     }
-    public static void task6 () {
-        System.out.println("Задача 6");
-        byte banana = 5;
-        byte bananaWeight = 80;
-        int allBananasWeight = banana * bananaWeight;
-        short milkTotal = 200;
-        byte milkPortion = 100;
-        byte milkWeightPortion = 105;
-        int amountMilkPortion = milkTotal / milkPortion;
-        int allMilkWeight = amountMilkPortion * milkWeightPortion;
-        byte iceCream = 2;
-        byte iceCreamWeight = 100;
-        int allIceCreamWeight = iceCream * iceCreamWeight;
-        byte egg = 4;
-        byte eggWeight = 70;
-        int allEggWeight = egg * eggWeight;
-        int sportsBreakfastWeight = allBananasWeight + allMilkWeight + allIceCreamWeight + allEggWeight;
-        System.out.println("Вес спортзавтрака " + sportsBreakfastWeight + " грамм.");
-        float sportsBreakfastWeightKilogram =  sportsBreakfastWeight / 1000f;
-        System.out.println("Вес спортзавтрака " + sportsBreakfastWeightKilogram + " килограмм.");
+    public static void task8 () {
+        System.out.println("Задача 8");
+        int salaryMasha = 67760;
+        int salaryDenis = 83690;
+        int salaryKristina = 76230;
+        double newSalaryMasha = salaryMasha * 1.1;
+        double newSalaryDenis = salaryDenis * 1.1;
+        double newSalaryKristina = salaryKristina * 1.1;
+        double differenceSalaryMasha = newSalaryMasha - salaryMasha;
+        System.out.println("Маша теперь получает " + newSalaryMasha + " рублей. Годовой доход вырос на " + differenceSalaryMasha + " рублей.");
+        double differenceSalaryDenis = newSalaryDenis - salaryDenis;
+        System.out.println ("Денис теперь получает " + newSalaryDenis + " рублей. Годовой доход вырос на " + differenceSalaryDenis + " рублей.");
+        double differenceSalaryKristina = newSalaryKristina - salaryKristina;
+        System.out.println("Кристина теперь получает " + newSalaryKristina + " рублей. Годовой доход вырос на " + differenceSalaryKristina + " рублей.");
     }
 }
