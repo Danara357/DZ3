32cac19f2950031d5877dfe2e8b27ddcbd998b0b
Мой первый коммит <dsbasanova@mail.ru>
2023-03-25 21:45:33 +0300

    Задача4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task4();
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+        byte MachinePerformance = 16;
+        byte time = 2;
+        int bottlesMinute = MachinePerformance/time;
+        int for20 = bottlesMinute*20;
+        System.out.println("За 20 минут машина произвела " +  for20 + " штук бутылок.");
+        int forDay = bottlesMinute*24*60;
+        System.out.println("За сутки машина произвела " +  forDay + " штук бутылок.");
+        int forThreeDays = bottlesMinute*3*24*60;
+        System.out.println("За 3 дня машина произвела " +  forThreeDays + " штук бутылок.");
+        int forMonth = forDay*30;
+        System.out.println("За месяц машина произвела " +  forMonth + " штук бутылок.");
     }
 }
