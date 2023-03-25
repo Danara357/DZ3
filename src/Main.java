d0f58186e809033211e2ed2ffe512725337021f0
Мой первый коммит <dsbasanova@mail.ru>
2023-03-25 21:18:14 +0300

    Задача3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task3();
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        byte studentsLp = 23;
+        byte studentsAs = 27;
+        byte studentsEa = 30;
+        int studentsAll = studentsLp + studentsAs + studentsEa;
+        short sheetsPaper = 480;
+        int studentsPaper = sheetsPaper/studentsAll;
+        System.out.println("На каждого ученика рассчитано " + studentsPaper + " листов бумаги.");
     }
 }
